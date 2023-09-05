84c55b2d286510a9c137c12fd2c5051a74b02030
Daniil-Solo <daniil.solo1723@gmail.com>
2023-09-05 12:01:49 +0500

    add base for sqlalchemy orm

---
+++ src/database.py
@@ -0,0 +1,20 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import MetaData
+from typing import AsyncGenerator
+
+from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
+
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+Base = declarative_base()
+
+metadata = MetaData()
+
+engine = create_async_engine(DATABASE_URL)
+async_session_maker = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
