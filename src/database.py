94b3d1874ddeed014f5065dd462fbb33058e0e6e
Daniil-Solo <daniil.solo1723@gmail.com>
2023-09-08 11:55:07 +0500

    add sync session for celery

--- src/database.py
+++ src/database.py
@@ -1,18 +1,24 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import MetaData
 from typing import AsyncGenerator
 
 from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
 
-DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+ASYNC_DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 Base = declarative_base()
 
 metadata = MetaData()
 
-engine = create_async_engine(DATABASE_URL)
-async_session_maker = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+async_engine = create_async_engine(ASYNC_DATABASE_URL)
+async_session_maker = async_sessionmaker(async_engine, class_=AsyncSession, expire_on_commit=False)
+
+engine = create_engine(DATABASE_URL)
+Session = sessionmaker(engine)
 
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
